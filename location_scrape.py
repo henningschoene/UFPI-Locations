294607bc95eb3aeef559f576b6f72dc264edcbce
henningschoene <hngschoene@gmail.com>
2021-05-23 16:46:45 -0600

    Save extracted data in all_locations list

--- location_scrape.py
+++ location_scrape.py
@@ -17,11 +17,23 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 # Find each search result
 results = soup.find_all(class_='location-card')
 
+# Create list to hold all location dictionaries
+all_locations = []
+
+# Loop over all locations
 for result in results:
+    # extract details for individual location into location_dict
     name = result.find(class_='result-card__title').text
     street = result.find(class_='field-street-address').text
     city = result.find(class_='field-city').text.rstrip(', ')
     zip = result.find(class_='field-zip').text
     state = result.find(class_='field-state-abbreviation').text
 
-    print(f'Name: {name}\nStreet: {street}\nCity: {city}\nZip: {zip}\nState: {state}\n')
+    location_dict = {'name': name, 'street': street, 'city': city, 'zip': zip, 'state': state}
+    # Append location_dict to all_locations
+    all_locations.append(location_dict)
+
+print(all_locations)
+
+# Close webdriver
+driver.quit()
