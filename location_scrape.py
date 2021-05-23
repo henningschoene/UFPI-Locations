548d8ad1a38479025225cb40643cdc186fcd058b
henningschoene <hngschoene@gmail.com>
2021-05-23 16:24:30 -0600

    Scraping locations website added

--- location_scrape.py
+++ location_scrape.py
@@ -1,8 +1,21 @@
-import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
+# Create firefox browser for selenium
+opts = webdriver.FirefoxOptions()
+# Turn headless browser on (True) or off (False)
+opts.headless = False
+driver = webdriver.Firefox(options=opts)
+driver.set_page_load_timeout(30)
 
-locations_url = 'https://www.ufpi.com/en/our-locations'
+url = 'https://www.ufpi.com/en/our-locations'
 
-locations = requests.get(locations_url)
+# Open and parse url
+driver.get(url)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-print(locations)
+# Find each search result
+results = soup.find_all(class_='location-card')
+
+# Print all search get_results
+print(results)
